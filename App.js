64d6bd00f4333023df0a3082597e73dc3a910d18
MouhaDiouf <mouhamadoudioufsn@gmail.com>
2021-05-25 09:38:41 +0000

    Build the StartGame screen

--- App.js
+++ App.js
@@ -2,11 +2,12 @@ import { StatusBar } from 'expo-status-bar';
 import React from 'react';
 import {  Text, View, StyleSheet } from 'react-native';
 import Header from './components/Header'
+import StartGameScreen from './screens/StartGameScreen';
 export default function App() {
   return (
     <View style={styles.screen}>
       <Header title='Guess The Number' />
-      
+      <StartGameScreen />
     </View>
   );
 }
